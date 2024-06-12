2a10ea80362d294827d5eb985c281bc744a9c869
unknown <magedfiverr@gmail.com>
2024-06-12 18:52:20 +0200

    add some funcionalities that read a html file and insert the data from the json file and create a new html file

---
+++ animals_web_generator.py
@@ -0,0 +1,65 @@
+import json 
+
+
+def load_data(file_path):
+    '''Loads json file'''
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+    
+
+def load_html(file_path):
+    '''Load html file'''
+    with open(file_path, "r") as handler:
+        html_data = handler.read()
+        return html_data
+
+
+def save_data(filename, data):
+    '''save data to new file'''
+    with open(filename, "w") as handler:
+        handler.write(data)
+
+
+def get_animal_facts(data):
+    '''Print data from animal dictionary'''
+    animal_data_string = ""
+    for animal in data:
+        try:
+            animal_data_string += f"name: {animal['name']}\n"
+        except KeyError:
+            pass
+        
+        try:
+            animal_data_string += f"diet: {animal['characteristics']['diet']}\n"
+        except KeyError:
+            pass
+        
+        try:
+            animal_data_string += f"Location: {animal['locations'][0]}\n"
+        except KeyError:
+            pass
+        
+        try:
+            animal_data_string += f"type: {animal['characteristics']['type']}\n"
+        except KeyError:
+            pass
+        animal_data_string += "\n\n"
+    return animal_data_string
+        
+
+
+def main():
+    # Load all the data 
+    animals_json = load_data('animals_data.json')
+    animal_facts = get_animal_facts(animals_json)
+    animals_html_file = load_html('animals_template.html')
+
+    # Replace somethin in animals_html_file
+    animals_html_file = animals_html_file.replace("__REPLACE_ANIMALS_INFO__", animal_facts)
+
+    # Save the file
+    save_data("animals.html", animals_html_file)
+
+
+if __name__ == "__main__":
+    main()
